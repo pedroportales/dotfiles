d5a05786f8defadef49381f5c8fbe2def1a9bdc6
Pedro Portales <pedroportales@riseup.net>
2022-05-24 22:13:36 -0300

    emacs autopair

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,6 +8,9 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
+;; Autopair
+(electric-pair-mode t)
+
 (defun efs/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
            (format "%.2f seconds"
