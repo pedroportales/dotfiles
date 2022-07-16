296e4f1030ecdcce3562a79b621e058d5adb1125
Pedro Portales <pedroportales@riseup.net>
2022-07-15 22:23:44 -0300

    install lisp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,13 @@
 require("paq")({
 	"savq/paq-nvim",
+	"williamboman/nvim-lsp-installer",
 	"neovim/nvim-lspconfig",
 	"nvim-treesitter/nvim-treesitter",
 	"numToStr/Comment.nvim",
 	"hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
+	"saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
 	"nvim-lualine/lualine.nvim",
 	'kyazdani42/nvim-tree.lua',
 	'kyazdani42/nvim-web-devicons',
@@ -13,4 +16,5 @@ require("paq")({
 })
 
 require("plugins-conf")
+require("lsp")
 vim.cmd[[colorscheme nord]]
