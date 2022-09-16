f9b2cf5169ea3eaafbcbf4fec47ea9dc45c202f3
Pedro Portales <pedroportales@riseup.net>
2022-09-16 20:31:36 -0300

    starting autostart

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -628,3 +628,15 @@ end)
 --client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 --client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+do
+    local cmds =
+    {
+        "~/.screenlayout/awesome-screen.sh",
+        "nm-applet"
+    }
+
+    for _,i in pairs(cmds) do
+        awful.util.spawn(i)
+    end
+end
